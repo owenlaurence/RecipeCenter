d1da4fbdffc75fc4310d5b848863463aec04e2c2
stu <eoghan.laurence.12@gmail.com>
2025-11-05 17:58:03 -0600

    Adding vercel metric tag.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 export const metadata: Metadata = {
   title: "Bread👍",
@@ -15,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SpeedInsights/>
       <body>
         {children}
       </body>
