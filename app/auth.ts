0c902f9ccf1cabfc9060aaa769a7bb1eae91d6f5
stu <eoghan.laurence.12@gmail.com>
2025-11-08 20:39:59 -0600

    fixing build

--- app/auth.ts
+++ app/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { Pool } from "pg";
 
 export const auth = betterAuth({
+
   database: new Pool({
     connectionString: process.env.DATABASE_URL
   }),
